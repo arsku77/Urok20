803dcaae0b24fdb590acadec757df6b69bc9e5e4
Arunas Skucas <arsku77@gmail.com>
2017-05-18 10:16:30 +0300

    StringHelper Dom zadanija 2

--- frontend/components/stringHelper.php
+++ frontend/components/stringHelper.php
@@ -15,35 +15,60 @@ class stringHelper
 
     public function __construct()
     {
-        $this->limit = Yii::$app->params['shortTextLimit'];
+        $this->limit = Yii::$app->params['wordsLimit'];
     }
-
+    
     /**
      * @param string $string
      * @param integer $limit
      * @return string
      */
     public function getShort($string, $limit = null)
-    {
+    {//veikia
         if($limit === null) {
-            $limit =  $this->limit;
-        }
-
+            $limit = $this->limit;
+           }
+ 
         $string = strip_tags($string);
-        $stringLen = strlen($string);
-        if ($stringLen > $limit) {
-            // get first string
-            $stringCutLimitFront = substr($string, 0, $limit);//front cut for limit 
-            $intLastSpaceForLimit = strrpos($stringCutLimitFront, ' ');//position last space in front cut
+        $intLimitWord = intval($limit);
+        $arrWords = array();
+        $arrWords = explode(' ', $string, $intLimitWord + 1);//get array of string: $limit+1 elements
 
-            // get last string
-            $stringCutLimitEnd = substr($string, $limit, $stringLen - $limit);// last cut for limit
-            $intFirstSpaceForLimit = stripos($stringCutLimitEnd, ' ');//position first space in last cut 
-            
-            // forming string
-            $string = substr($string, 0, $limit + $intFirstSpaceForLimit).'...'; //add quantity for one word
+        if(count($arrWords) == ($intLimitWord+1)){// is last elements for $arrWords
+           $arrWords[$intLimitWord] = " ... "; //replace..
         }
-        return $string;    
+        
+        return implode(' ', $arrWords);//get string of array
+        
+    }
+//    /**
+//     * @param string $string
+//     * @param integer $limit
+//     * @return string
+//     */
+//    public function getShort($string, $limit = null)
+//    {
+//        if($limit === null) {
+//            $limit =  $this->limit;
+//        }
+//
+//        $string = strip_tags($string);
+//        $stringLen = strlen($string);
+//        if ($stringLen > $limit) {
+//            // get first string
+//            $stringCutLimitFront = substr($string, 0, $limit);//front cut for limit 
+//            $intLastSpaceForLimit = strrpos($stringCutLimitFront, ' ');//position last space in front cut
+//
+//            // get last string
+//            $stringCutLimitEnd = substr($string, $limit, $stringLen - $limit);// last cut for limit
+//            $intFirstSpaceForLimit = stripos($stringCutLimitEnd, ' ');//position first space in last cut 
+//            
+//            // forming string
+//            $string = substr($string, 0, $limit + $intFirstSpaceForLimit).'...'; //add quantity for one word
+//        }
+//        return $string;    
+//
+//    }
 
-}
+    
 }
