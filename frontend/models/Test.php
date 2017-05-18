00f6e0ddaea30051f290482a709c9434c3e2f648
Arunas Skucas <arsku77@gmail.com>
2017-05-18 12:41:52 +0300

    Kolicestv Novost Dom zad 1

--- frontend/models/Test.php
+++ frontend/models/Test.php
@@ -10,6 +10,23 @@ use Yii;
 class Test
 {
     /**
+     * @return modified array
+     */
+    public static function getAllNewsList()
+    {
+        $sql = 'SELECT * FROM news' ;
+        $result = Yii::$app->db->createCommand($sql)->queryAll();
+
+
+        if(!empty($result) && is_array($result)) {
+            foreach ($result as &$item) {//žemiau yra mūs sukurtas komponentas
+                $item['content'] = Yii::$app->stringHelper->getShort($item['content']);
+            }
+        }
+        return $result;//grąžinam jau modifikuotą masyvą - jame pakeistas turinys content
+    }
+
+    /**
      * @param integer $max
      * @return array
      */
